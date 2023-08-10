8afa4043d0839e3ce5ca690c4fe38ab3deb77381
Nesmaz <nesmabdelazim@gmail.com>
2023-08-10 08:21:00 -0700

    C program to print text1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program to print with printf function
+ * Return: 0 indicates success
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}

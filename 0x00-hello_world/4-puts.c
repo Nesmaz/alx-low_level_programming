77919e37413a99bc597ad3ad8b36f1b844489e89
Nesmaz <nesmabdelazim@gmail.com>
2023-08-10 08:06:22 -0700

    C program to print text!

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: C program to print with puts function
+ * Return: 0 indicates success
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+		return (0);
+}

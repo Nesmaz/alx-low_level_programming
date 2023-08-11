2cd46fc75eb2fc1235acc14b69010eba4410f182
Nesmaz <nesmabdelazim@gmail.com>
2023-08-11 16:03:50 -0700

    Printing all possible combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description : print random number with printf function
+ * Return: 0 if successful, non-zero otherwise
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

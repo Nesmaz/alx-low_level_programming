370eb75c5346188121774736a109a7ba42db0e96
Nesmaz <nesmabdelazim@gmail.com>
2023-08-11 07:43:19 -0700

    Assigning a random value to a variable every execution

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description : printint random number with printf function
+ *
+ * Return : 0 indicates success
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if(n < 0){
+		printf("%d : is negative\n", n);
+	}else if (n > 0){
+		printf("%d : is positive\n",n);
+	}else{
+		printf("%d : is zero\n", n);
+	}
+	return (0);
+}

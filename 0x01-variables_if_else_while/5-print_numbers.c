71fa81d8604516bbae289a69795e7a3bcb40ab73
Nesmaz <nesmabdelazim@gmail.com>
2023-08-11 10:25:15 -0700

    Prointing numbers of base 10 from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description : print digit numbers of base 10 from 0 with printf function
+ * Return: 0 if successful, non-zero otherwise
+ */
+
+int main(void)
+{	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}

c84e21a4bc874f6d9fcaa0c1e5f4d12f8bec1b14
Nesmaz <nesmabdelazim@gmail.com>
2023-08-15 19:54:18 -0700

    Quick fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,32 +9,34 @@ void print_times_table(int n)
 {
 	int row, col, count;
 
-	if (n >= 0 && n <= 15)
+	if (n <= 15 && n >= 0)
 	{
-		for (row = 0; row <= n; row++)
+		for (row = 0; row <= n; ++row)
 		{
 			_putchar('0');
-			for (col = 1; col <= n; col++)
+			for (col = 1; col <= n; ++col)
 			{
 				_putchar(',');
 				_putchar(' ');
 
 				count = row * col;
 
-				if (count <= 10)
+				if (count <= 9)
 				{
-					_putchar(' ');
 					_putchar(' ');
 				}
-
-				else if (count < 100)
+				if (count <=99)
 				{
 					_putchar(' ');
+				}
+				else if (count >= 100)
+				{
+					_putchar((count / 1000 + '0');
 					_putchar((count / 10) + '0');
 				}
-				else
+				else if (count <= 99 && count >= 10)
 				{
-					_putchar((count / 100) + '0');
+
 					_putchar((count / 10) % 10 + '0');
 				}
 

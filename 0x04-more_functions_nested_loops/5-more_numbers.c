98f79aef19a24dac5747aa5b0c7951a1c12c209b
Nesmaz <nesmabdelazim@gmail.com>
2023-08-17 13:28:07 -0700

    Quick fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,41 +2,27 @@
 #include "main.h"
 
 /**
- * print_2digits - prints numbers greater than 9
- * Description: a function that prints a number of 2 digits
- * @num: number greater than 9 to be printed
-*/
-
-void print_2digits(int num)
-{
-	int tens = num / 10;
-	int ones = num % 10;
-
-	_putchar(tens + '0');
-	_putchar(ones + '0');
-}
-/**
  * more_numbers - prints 10 times the numbers from 0 to 14
- *		using _putchar and print_2digits functions
+ *		using _putchar function and nested for loops
 */
 
 void more_numbers(void)
 {
-	int row, col;
+	int row, col, num;
 
 	for (row = 0; row <= 9; row++)
 	{
 		for (col = 0; col <= 14; col++)
 		{
-			if (col <= 9)
+			num = col;
+
+			if (col > 9)
 			{
-				_putchar(col + '0');
-			}
-			else
-			{
-				print_2digits(col);
+				_putchar(1 + '0');
+				num = col % 10;
 			}
 
+			_putchar(num + '0');
 		}
 			_putchar('\n');
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,7 +16,6 @@ void print_most_numbers(void);
 
 void more_numbers(void);
 
-void print_2digits(int num);
 
 #endif/* MAIN-H */
 

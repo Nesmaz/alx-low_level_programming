5daf049983301ef97f8d4a3e1f83cf0192adf016
Nesmaz <nesmabdelazim@gmail.com>
2023-09-11 16:41:06 -0700

    quick fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,8 +15,8 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: ");
-		if (d->name == NULL) 
-		       	printf("(nil)\n");
+		if (d->name == NULL)
+			printf("(nil)\n");
 		else
 			printf("%s\n", d->name);
 		printf("Age: %f\n", d->age);

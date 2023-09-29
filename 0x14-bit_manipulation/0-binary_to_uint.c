dca627196b63bfd7db8390ee97e899daa9494678
Nesmaz <nesmabdelazim@gmail.com>
2023-09-28 17:13:00 -0700

    quick fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,20 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, num = 0;
+	unsigned int i, p, result;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; b[i]; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		num <<= 1;
-		if (b[i] == '1')
-			num += 1;
 	}
-	return (num);
+	for (p = 1, result = 0; i > 0; i--, p *= 2)
+	{
+		if (b[i - 1] == '1')
+			result += p;
+	}
+	return (result);
 }
